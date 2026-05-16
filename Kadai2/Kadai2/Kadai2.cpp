d0f0a2e404fb9ab9eaaddec16d4e87c2d55f118e
Mona0509 <ktc25a31a0056@edu.kyoto-tech.ac.jp>
2026-05-16 16:16:21 +0900

    Kadai2完成

--- Kadai2/Kadai2/Kadai2.cpp
+++ Kadai2/Kadai2/Kadai2.cpp
@@ -3,6 +3,58 @@
 
 #include <iostream>
 #include <stdio.h>
+
+// name,score　を持つ構造体を定義
+struct StudentProgram {
+	char name;
+	int score;
+};
+int OpenAnswer(int perfectAns, int averageAns, char perfectStudent);
+int checkNum = 0;	// 点数比較用
+int answerNum = 0;	// 最高点
+int hNum = 0;		// 平均点	
+int nameNum = 0;	// 最高点の生徒チェック用	
 int main()
 {
+	StudentProgram student_1
+	{'A',58};
+	StudentProgram student_2
+	{ 'B',47 };
+	StudentProgram student_3
+	{ 'C',89 };
+	StudentProgram student_4
+	{ 'D',90 };
+	StudentProgram student_5
+	{ 'E',36 };
+	int box[5]{
+		student_1.score,
+		student_2.score,
+		student_3.score,
+		student_4.score,
+		student_5.score,
+	};
+	int boxName[5]{
+		student_1.name,
+		student_2.name,
+		student_3.name,
+		student_4.name,
+		student_5.name,
+	};
+	for (int i = 0; i < 5; i++) {
+		hNum += box[i];			// 合計を求める
+		if (checkNum < box[i]) {
+			checkNum = box[i];
+			nameNum = i;
+		}
+	}
+	answerNum = checkNum;		// 最高点の変数移動
+	int answerH = hNum / 5;		// 平均点計算
+	OpenAnswer(answerNum,answerH,boxName[nameNum]);		// (最高点,平均点,最高得点者)
+	return 0;
+}
+int OpenAnswer(int perfectAns, int averageAns, char perfectStudent) {
+	printf("最高点: %d\n", perfectAns);	// 最高点表示
+	printf("平均点: %d\n", averageAns);	// 平均点表示
+	printf("最高得点者: %c\n", perfectStudent);	// 最高得点者の名前表示
+	return 0;
 }
