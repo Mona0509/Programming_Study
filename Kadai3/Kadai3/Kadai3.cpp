21168da1c7b6389878ce8b654a24c8bc2da3b11f
Mona0509 <ktc25a31a0056@edu.kyoto-tech.ac.jp>
2026-07-18 16:29:31 +0900

    未完成(ビルドエラーあり)

---
+++ Kadai3/Kadai3/Kadai3.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+template<class T>
+bool myFind(T v) {
+    return (v % 2) != 0;
+}
+
+// クラスA
+class ClassA
+{
+public:
+    // コンストラクタ
+    ClassA(int type) :type_(type) {}
+
+public:
+    // タイプ取得
+    int type() const { return type_; }
+
+private:
+    int type_ = 0;
+};
+
+// クラスB
+class ClassB
+{
+public:
+    // コンストラクタ
+    ClassB(int type) :type_(type) {}
+
+public:
+    // タイプ取得
+    int type() const { return type_; }
+
+private:
+    int type_ = 0;
+};
+
+
+// エントリー関数
+int main()
+{
+    // クラスAのベクタ
+    std::vector<ClassA> aVector;
+    aVector.emplace_back(1);
+    aVector.emplace_back(2);
+    aVector.emplace_back(0);
+    aVector.emplace_back(3);
+
+
+    // クラスBのベクタ
+    std::vector<ClassB> bVector;
+    bVector.emplace_back(0);
+    bVector.emplace_back(2);
+    bVector.emplace_back(1);
+    bVector.emplace_back(3);
+
+    bool findA = myFind(aVector);
+    if (findA)
+    {
+        std::cout << "aVector に 「1」 があった" << std::endl;
+    }
+
+    bool findb = myFind(bVector);
+    if (findb)
+    {
+        std::cout << "bVector に 「1」 があった" << std::endl;
+    }
+}
